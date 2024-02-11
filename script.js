0295ceb7f8ba33ec446c4ba931a87755f570128c
gwang1210 <gary.wang@wustl.edu>
2024-02-11 16:11:44 -0600

    Da Real MVP

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let openHam = document.querySelector('#openHam');
-let closeHam = document.querySelector('#closeHam');
-let navigationItems = document.querySelector('#navigation-items');
+// let openHam = document.querySelector('#openHam');
+// let closeHam = document.querySelector('#closeHam');
+// let navigationItems = document.querySelector('#navigation-items');
 
-const hamburgerEvent = (navigation, close, open) => {
-    navigationItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
-};
+// const hamburgerEvent = (navigation, close, open) => {
+//     navigationItems.style.display = navigation;
+//     closeHam.style.display = close;
+//     openHam.style.display = open;
+// };
 
-openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
-closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+// openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
+// closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
+
+// // let elementsArray = document.querySelectorAll("a.cs-li-link");
+
+// // var elements = document.getElementsByTagName('a');
+// // for(var i = 0, len = elements.length; i < len; i++) {
+// //     elements[i].onclick = function () {
+// //         navigationItems.style.display = "none";
+// //         closeHam.style.display = "none";
+// //         openHam.style.display = "block";
+// //     }
+// // }
+
+// let test = document.querySelector('#test');
+// test.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
